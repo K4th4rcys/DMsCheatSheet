12b5fcd54826517bafa4d466a657f7adc0dd7a05
Neegat910 <riku910@gmail.com>
2018-01-31 15:31:54 -0600

    Delete main.js

--- DMsCheatSheet/resources/js/main.js
+++
@@ -1,7 +0,0 @@
-
-$(document).ready(function() {
-      $("#intro_img").css("left", (($(window).width()/2)-($("#intro_img").width()/2)));
-      // console.log(($(window).width()/2)-($("#intro_img").width()/2));
-      $(".mainlink").css("height", ($("#mainmenu").height()*(.20)));
-      $(".mainlink").css("width", ($(".shadow").width()));
-});
