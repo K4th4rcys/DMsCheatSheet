1aa13901508868a4aa7cf74316e949a663325d86
Neegat910 <riku910@gmail.com>
2018-02-04 11:06:17 -0600

    Resources delete

--- Resources/modules/sidemenu.php
+++
@@ -1,8 +0,0 @@
-<div id="side_menu">
-  <div id="logo">
-  </div>
-  <table id="menu_links">
-    <tr class
-  </table>
-
-</div>
--- Resources/php/header.php
+++
@@ -1,9 +0,0 @@
-<?php
-  session_start();
-  // echo("header here");
-  // if(!isset($_SESSION['user'])) {
-  // header("Location: /login.php");
-  // }
-  exit;
-
- ?>
