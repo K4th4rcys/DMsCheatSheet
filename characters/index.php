3047fcc8c148dae59449412f6f796b0209793fbc
Maxwell Gariety <MaxwellG@corp.custom-pak.com>
2018-02-22 07:55:44 -0600

    85tottoiuiouyjklykjl

--- characters/index.php
+++ characters/index.php
@@ -31,7 +31,7 @@ Maxwell Gariety. This includes on personal projects.
         <div id="charopt_tabs">
           <button class="char_tab active_tab" id="pc">PCs</button>
           <button class="char_tab" id="npc">NPCs</button>
-          <button class="char_tab" id="boss">Bosses</buttondiv>
+          <button class="char_tab" id="boss">Bosses</button>
         </div>
         <div class="tables active_table" id="pc_table">
           <table id="pcs">
@@ -63,40 +63,71 @@ Maxwell Gariety. This includes on personal projects.
 
         </div>
       </div>
-      <div id="charsheet">
-        <table id="character_sheet">
-          <tr>
-            <th id="charname">
+      <div id="sheet_info">
+      <div class="sheet_type active_sheet_type" id="pc_sheet">
+        <div id="char_sheets_tabs">
+          <button class="sheet_tab active_sheet_tab" id="stats_tab">Stats</button>
+          <button class="sheet_tab" id="inventory_tab">Inventory</button>
+          <button class="sheet_tab" id="combat_tab">Combat</button>
+          <button class="sheet_tab" id="powers_tab">Powers</button>
+          <button class="sheet_tab" id="features_tab">Features</button>
+        </div>
+        <div class="char_sheets active_sheet_table" id="stats_table">
+          <table id="stats_tb">
+              <tr id="pc_sheet_header">
+                <th id="charname">
 
-            </th>
-            <th id="charlevel">
+                </th>
+                <th id="charlevel">
 
-            </th>
-            <th id="charrace">
+                </th>
+                <th id="charrace">
 
-            </th>
-            <th id="charclass">
+                </th>
+                <th id="charclass">
 
-            </th>
-            <th id="background">
+                </th>
+                <th id="background">
 
-            </th>
-          </tr>
-          <tr>
-            <td id="">
+                </th>
+              </tr>
+              <tr>
+                <td id="">
 
-            </td>
-            <td id="">
+                </td>
+                <td id="">
 
-            </td>
-            <td id="">
+                </td>
+                <td id="">
 
-            </td>
-            <td id="">
+                </td>
+                <td id="">
 
-            </td>
-          </tr>
-        </table>
+                </td>
+              </tr>
+          </table>
+        </div>
+        <div class="char_sheets" id="inventory_table">
+          <table id="inventory_tab">
+
+          </table>
+        </div>
+        <div class="char_sheets" id="combat_table">
+          <table id="combat_tab">
+
+          </table>
+        </div>
+        <div class="char_sheets" id="powers_tab">
+          <table id="powers_tab">
+
+          </table>
+        </div>
+        <div class="char_sheets" id="features_tab">
+          <table id="features_tab">
+
+          </table>
+        </div>
+      </div>
       </div>
   </div>
 
